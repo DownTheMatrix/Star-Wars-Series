7a8d115a2fa57a99f7225be5af0709e3f73932f8
Bruno Mazza <suija87@gmail.com>
2018-02-13 12:41:36 +0100

    Create main.js

---
+++ buttons/main.js
@@ -0,0 +1,9 @@
+document.getElementById('rebel-fanfare').addEventListener('click', music);
+
+ function music() {
+  let audioElem = document.getElementById("march");
+   if (audioElem.paused) audioElem.play();
+  else audioElem.pause();
+}
+  
+
