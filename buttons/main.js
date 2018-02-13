84f992107ae543b7e95c9ae65ebfd6a00f0ce1ec
Bruno Mazza <suija87@gmail.com>
2018-02-13 12:59:17 +0100

    Delete main.js

--- buttons/main.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('rebel-fanfare').addEventListener('click', music);
-
- function music() {
-  let audioElem = document.getElementById("march");
-   if (audioElem.paused) audioElem.play();
-  else audioElem.pause();
-}
-  
-
